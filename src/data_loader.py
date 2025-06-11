4b7e4cad6142f32adf2ddf58b4ee425c603e046d
Aditi Sarathy <asarathy@intersystems.com>
2025-06-11 12:05:24 -0400

    load data into vector db

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,14 +2,16 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 import chromadb
 from chromadb.config import Settings
+import ast
 
 # loads our final project dataset from HuggingFace
 def load_dataset_from_hf():
     # Grab dataset CSV from Aditi's repository
     dataset_url = "https://huggingface.co/datasets/asarathy/patient_encounters1_notes/raw/main/patient_encounters1_notes.csv"
     df = pd.read_csv(dataset_url, on_bad_lines="warn")
+
     df['CLINICAL_NOTES_NONULL'] = df['CLINICAL_NOTES'].fillna('')
-    df['CLINICAL_NOTES_LEMMATIZED_JOINED'] = df['CLINICAL_NOTES_CLEAN_LEMMATIZED'].map(lambda x: ' '.join(x))
+    df['CLINICAL_NOTES_LEMMATIZED_JOINED'] = df['CLINICAL_NOTES_CLEAN_LEMMATIZED'].apply(lambda x: ' '.join(ast.literal_eval(x)) if isinstance(x, str) else '')
     return df
 
 # Loads data from the given dataframe into the vector DB, as in workshop 7.
@@ -30,3 +32,5 @@ def load_data_into_vectordb(df, data_column = "CLINICAL_NOTES_NONULL"):
         embeddings=embeddings,
         ids=[str(i) for i in range(len(texts))]
     )
+
+    
