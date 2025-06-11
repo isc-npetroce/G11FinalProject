fa05c0231a8040ac729255bedb51afadace14090
Nicholas Petrocelli <npetroce@intersystems.com>
2025-06-11 11:05:04 -0400

    Initial commit of data loader, Aditi to test

---
+++ src/data_loader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+import chromadb
+from chromadb.config import Settings
+
+# loads our final project dataset from HuggingFace
+def load_dataset_from_hf():
+    # Grab dataset CSV from Aditi's repository
+    dataset_url = "https://huggingface.co/datasets/asarathy/patient_encounters1_notes/blob/main/patient_encounters1_notes.csv"
+    df = pd.read_csv(dataset_url)
+    df['CLINICAL_NOTES_NONULL'] = df['CLINICAL_NOTES'].fillna('')
+    df['CLINICAL_NOTES_LEMMATIZED_JOINED'] = df['CLINICAL_NOTES_CLEAN_LEMMATIZED'].map(lambda x: ' '.join(x))
+    return df
+
+# Loads data from the given dataframe into the vector DB, as in workshop 7.
+def load_data_into_vectordb(df, data_column = "CLINICAL_NOTES_NONULL"):
+    # starting with all-MiniLM as this was used in week 7
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    client = chromadb.Client(Settings(
+        persist_directory="./chroma_db"
+    ))
+    
+    collection = client.get_or_create_collection(name=data_column)
+
+    texts = df[data_column].astype(str).tolist()
+    embeddings = model.encode(texts).tolist()
+
+    collection.add(
+        documents=texts,
+        embeddings=embeddings,
+        ids=[str(i) for i in range(len(texts))]
+    )
