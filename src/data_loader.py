453ee11ef3170f26a9a932c2ee1a1ca64acb71ac
Nick Petrocelli <npetroce@intersystems.com>
2025-06-13 11:46:52 -0400

    Idempotent data loader and utility methods, plus comments

--- src/data_loader.py
+++ src/data_loader.py
@@ -10,15 +10,17 @@ def load_dataset_from_hf():
     dataset_url = "https://huggingface.co/datasets/asarathy/patient_encounters1_notes/raw/main/patient_encounters1_notes.csv"
     df = pd.read_csv(dataset_url, on_bad_lines="warn")
 
+    # get two potential data columns
+    # first is just the basic clinical notes with nulls replaced by emptystring
     df['CLINICAL_NOTES_NONULL'] = df['CLINICAL_NOTES'].fillna('')
+    # second is our cleaned notes from Week 6. Unfortunately our tokenization breaks the input format for the sentence transformer,
+    # so we naively join the cleaned text back into single documents by joining on ' '.
     df['CLINICAL_NOTES_LEMMATIZED_JOINED'] = df['CLINICAL_NOTES_CLEAN_LEMMATIZED'].apply(lambda x: ' '.join(ast.literal_eval(x)) if isinstance(x, str) else '')
     return df
 
 # Loads data from the given dataframe into the vector DB, as in workshop 7.
-# TODO add metadata for filtering, at least on patient name
-# TODO delete data from db before re-adding
 # https://docs.trychroma.com/docs/collections/delete-data
-def load_data_into_vectordb(df, data_column = "CLINICAL_NOTES_NONULL"):
+def load_data_into_vectordb(df, data_column = "CLINICAL_NOTES_NONULL", metadata_columns = ['PATIENT_ID', 'FIRST', 'ENCOUNTER_ID']):
     # starting with all-MiniLM as this was used in week 7
     model = SentenceTransformer("all-MiniLM-L6-v2")
     client = chromadb.Client(Settings(
@@ -28,12 +30,32 @@ def load_data_into_vectordb(df, data_column = "CLINICAL_NOTES_NONULL"):
     collection = client.get_or_create_collection(name=data_column)
 
     texts = df[data_column].astype(str).tolist()
+    # gets dict of metadata fields in 'records' format, which is what chroma expects
+    # i.e. [{'index': 0, 'patient_id': 1, 'first': 'foo'}, {'index':1, 'patient_id':2, 'first': bar}]
+    metadatas = df[metadata_columns].to_dict('records')
+
     embeddings = model.encode(texts).tolist()
 
-    collection.add(
+    # use 'upsert' instead of add
+    # this updates existing documents by ID and inserts if they do not exist
+    collection.upsert(
         documents=texts,
         embeddings=embeddings,
+        metadatas=metadatas,
         ids=[str(i) for i in range(len(texts))]
     )
+    # for convenience return the collection for querying
+    return collection
+
+
+# utility to get connection to chromaDB
+# returns both client and collection object
+# will throw exception if load_data_into_vectordb has not been called at least once
+def get_chroma_client_and_collection(data_column = "CLINICAL_NOTES_NONULL"):
+    client = chromadb.Client(Settings(
+        persist_directory="./chroma_db"
+    ))
+    collection = client.get_collection(name=data_column)
+    return client, collection
 
     
